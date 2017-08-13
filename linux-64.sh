90d84b77917e8dc96cacb6446c77f65187666c9d
Anthony Federico <anfederico@users.noreply.github.com>
2017-08-13 14:23:18 -0400

    Update linux-64.sh

--- linux-64.sh
+++ linux-64.sh
@@ -8,26 +8,22 @@ gedit ~/.bashrc
 
 # Create a new environment
 cd ~/miniconda3/envs
-conda create -n pypliner3 python=3.5
+conda create -n pipeliner python=2.7
 
 # Make sure you see it in the correct location
 conda info --envs
 
 # Activate it
-source activate pypliner3
+source activate pipeliner
 
 # Install dependencies
-conda install -c bioconda java-jdk
-conda install -c bioconda fastqc
-conda install -c bioconda trim-galore
-conda install -c bioconda star
-conda install -c bioconda multiqc
+conda install -c bioconda java-jdk fastqc trim-galore star multiqc samtools rseqc stringtie
 
 # Make sure Java 7/8 is installed
 java -version
 
 # Install Nextflow executable
-cd ~/Documents/pythonvillage/pypliner3/Gallus_example
+cd ~/Documents/rnaseq/pipeliner/Gallus_example
 curl -s https://get.nextflow.io | bash
 
 # Test Nextflow executable
