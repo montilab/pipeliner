f40b4975c393322bc42edf1cf6914300da30975f
Anthony Federico <anfederico@users.noreply.github.com>
2017-10-14 18:06:30 -0400

    Delete stringtie2.sh

--- Gallus_gallus/templates/stringtie2.sh
+++
@@ -1,8 +0,0 @@
-stringtie ${bamfiles} \\
--o ${bamfiles}_transcripts.gtf \\
--v \\
--G ${mergedgtf} \\
--A ${bamfiles}.gene_abund.txt \\
--C ${bamfiles}.cov_refs.gtf \\
--e \\
--b ${bamfiles}_ballgown
