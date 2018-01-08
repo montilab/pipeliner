96998860e5e081cac8f7710b4fcfc631145d0dcd
Anthony Federico <anfederico@users.noreply.github.com>
2018-01-08 16:52:30 -0500

    Update docker.sh

--- docs/docker.sh
+++ docs/docker.sh
@@ -1,27 +1,27 @@
 # Find list of docker repositories
-docker images
+$ docker images
 
 # Using the miniconda fresh docker image
-docker pull continuumio/miniconda
-docker run -i -t continuumio/miniconda /bin/bash
+$ docker pull continuumio/miniconda
+$ docker run -i -t continuumio/miniconda /bin/bash
 
 # But you could also just do the following for any repository
-docker run -i -t <repository> /bin/bash
+$ docker run -i -t <repository> /bin/bash
 
 # Docker image is now activating, can access conda environment from here
-conda list
+$ conda list
 
 # Install your packages
-conda install --channel "anfederico" trim-galore fastqc star multiqc samtools rseqc stringtie hisat2 htseq subread numpy pandas bioconductor-biobase
+$ conda install --channel "anfederico" trim-galore fastqc star multiqc samtools rseqc stringtie hisat2 htseq subread numpy pandas bioconductor-biobase
 
 # while in the docker image, grab the id (looks like c16378f943fe)
 # Create a new repository with updated conda environment (copy)
-docker commit c16378f943fe <repository>
+$ docker commit c16378f943fe <repository>
 
-docker login
-docker images
-docker tag bb38976d03cf anfederico/pipeliner:<repository>
-docker push anfederico/pipeliner
+$ docker login
+$ docker images
+$ docker tag bb38976d03cf anfederico/pipeliner:<repository>
+$ docker push anfederico/pipeliner
 
 # To download on new computer
-docker pull anfederico/<repository>
+$ docker pull anfederico/<repository>
