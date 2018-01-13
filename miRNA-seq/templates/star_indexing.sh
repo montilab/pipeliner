1ca94f53aa03fc7333b2b38e7ebb6b126f68e628
Anthony Federico <anfederico@users.noreply.github.com>
2018-01-13 09:36:47 -0500

    Delete star_indexing.sh

--- miRNA-seq/templates/star_indexing.sh
+++
@@ -1,7 +0,0 @@
-mkdir star_index
-STAR --runMode genomeGenerate \\
---runThreadN ${task.cpus} \\
---sjdbGTFfile ${gtf} \\
---genomeDir star_index/ \\
---genomeFastaFiles ${fasta} \\
---sjdbOverhang ${params.star_indexing.sjdbOverhang}
